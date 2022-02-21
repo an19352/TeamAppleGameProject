1fec68a957a060436a39d64dd9cc3fb4f8239352
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-02-21 15:46:06 +0200

    Photon cannon stuff

--- Assets/Scripts/ImpulseCannon.cs
+++ Assets/Scripts/ImpulseCannon.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 
 public class ImpulseCannon : MonoBehaviour
 {
+    PhotonView PV;
+
     GameObject[] pushed;
     Transform parentPlayer;
     Rigidbody rigid;
@@ -15,25 +17,33 @@ public class ImpulseCannon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!PV.IsMine) return;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             pushed = GameObject.FindGameObjectsWithTag("Detected");
             for (int i = 0; i < pushed.Length; i++)
             {
-                rigid = pushed[i].transform.parent.GetComponent<Rigidbody>();
-                parentPlayer = transform.parent;
-                distance = Vector3.Distance(parentPlayer.position, pushed[i].transform.position);
-                rigid.AddForce(transform.forward * pushForce * (1 / distance), ForceMode.Impulse);
+                PV.RPC("RPC_Cannon", RpcTarget.All, i);
             }
         }
     }
     
+    [PunRPC]
+    void RPC_Cannon(int i)
+    {
+        rigid = pushed[i].transform.parent.GetComponent<Rigidbody>();
+        parentPlayer = transform.parent;
+        distance = Vector3.Distance(parentPlayer.position, pushed[i].transform.position);
+        rigid.AddForce(transform.forward * pushForce * (1 / distance), ForceMode.Impulse);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Detector"))
