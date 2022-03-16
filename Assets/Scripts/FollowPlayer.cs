35ca6671d25ac4c6974f0e68434535a73093ba54
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-03-16 13:59:26 +0200

    Added smooth option

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -9,14 +9,33 @@ public class FollowPlayer : MonoBehaviour
 
     public Vector3 offset;
     public float Zoffset;
+    public bool smoothMotion = false;
+    public float smoothSpeed = 1.25f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (player == null) return;
 
+        if (smoothMotion)
+            MoveCameraSmoothly();
+        else
+            MoveCamera();
+    }
+
+    void MoveCamera() 
+    {
         Vector3 newpos = new Vector3(player.position.x - offset.x, player.position.y - offset.y, player.position.z - offset.z);
         transform.position = newpos;
+
+        transform.LookAt(new Vector3(player.position.x, player.position.y, player.position.z + Zoffset));
+    }
+
+    void MoveCameraSmoothly()
+    {
+        Vector3 newpos = new Vector3(player.position.x - offset.x, player.position.y - offset.y, player.position.z - offset.z);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, newpos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPosition;
 
         transform.LookAt(new Vector3(player.position.x, player.position.y, player.position.z + Zoffset));
     }
