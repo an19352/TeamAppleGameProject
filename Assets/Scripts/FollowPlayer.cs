a55505691baa22d279a65e080ebfd6ed4a5b3d62
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-03-16 13:51:32 +0200

    Changed the camera a tad

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,14 +7,17 @@ public class FollowPlayer : MonoBehaviour
     [HideInInspector]
     public Transform player;
 
-    public Vector2 offset;
+    public Vector3 offset;
+    public float Zoffset;
 
     // Update is called once per frame
     void Update()
     {
         if (player == null) return;
 
-        Vector3 newpos = new Vector3(player.position.x - offset.x, transform.position.y, player.position.z - offset.y);
+        Vector3 newpos = new Vector3(player.position.x - offset.x, player.position.y - offset.y, player.position.z - offset.z);
         transform.position = newpos;
+
+        transform.LookAt(new Vector3(player.position.x, player.position.y, player.position.z + Zoffset));
     }
 }
