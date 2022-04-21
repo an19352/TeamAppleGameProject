c6bb0835795e483bf5a56455db05e6be45f98077
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-04-21 19:37:39 +0100

    Solved the pair added twice bug

--- Assets/StickObjectToMe.cs
+++ Assets/StickObjectToMe.cs
@@ -1,30 +1,31 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
 
-// public class StickObjectToMe : MonoBehaviour
-// {
-//     Dictionary<Transform, Transform> oldParentPhoneBook = new Dictionary<Transform, Transform>();
+ public class StickObjectToMe : MonoBehaviour
+ {
+     Dictionary<Transform, Transform> oldParentPhoneBook = new Dictionary<Transform, Transform>();
 
-//     private void OnCollisionEnter(Collision collision)
-//     {
-//         Transform target = collision.collider.transform;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform target = collision.collider.transform;
+        if (oldParentPhoneBook.ContainsKey(target)) return;
 
-//         if (target.Equals(GameMechanics.gameMechanics.GetLocalPlayer().transform)) Camera.main.transform.SetParent(transform);
+        if (target.Equals(GameMechanics.gameMechanics.GetLocalPlayer().transform)) Camera.main.transform.SetParent(transform);
 
-//         oldParentPhoneBook.Add(target, target.parent);
-//         target.SetParent(transform);
-//     }
+        oldParentPhoneBook.Add(target, target.parent);
+        target.SetParent(transform);
+    }
 
-//     private void OnCollisionExit(Collision collision)
-//     {
-//         Transform other = collision.collider.transform;
-//         if (oldParentPhoneBook.ContainsKey(other))
-//         {
-//             other.SetParent(oldParentPhoneBook[other]);
-//             if (other.Equals(GameMechanics.gameMechanics.GetLocalPlayer().transform)) Camera.main.transform.SetParent(oldParentPhoneBook[other]);
-//             oldParentPhoneBook.Remove(other);
-//         }
-//     }
-// }
+    private void OnCollisionExit(Collision collision)
+     {
+         Transform other = collision.collider.transform;
+         if (oldParentPhoneBook.ContainsKey(other))
+         {
+             other.SetParent(oldParentPhoneBook[other]);
+             if (other.Equals(GameMechanics.gameMechanics.GetLocalPlayer().transform)) Camera.main.transform.SetParent(oldParentPhoneBook[other]);
+             oldParentPhoneBook.Remove(other);
+         }
+     }
+ }
