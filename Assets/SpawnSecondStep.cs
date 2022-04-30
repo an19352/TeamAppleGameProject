9f9ef6159bed9646cded5b48482607952c4b5a6e
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-04-30 15:52:46 +0100

    Added a testing box

--- Assets/SpawnSecondStep.cs
+++ Assets/SpawnSecondStep.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SpawnSecondStep : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class SpawnSecondStep : MonoBehaviour
 
     public void SpawnObject()
     {
+        PhotonNetwork.Instantiate(spawns[0].obj.name, Vector3.zero, Quaternion.identity);
         return;
     }
 }
