f5f52e4a6ad66002846ad554da351a2f5708b910
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2022-04-30 16:13:57 +0100

    Let's try

--- Assets/Scripts/SpawnSecondStep.cs
+++ Assets/Scripts/SpawnSecondStep.cs
@@ -11,14 +11,13 @@ public class SpawnSecondStep : MonoBehaviour
         public GameObject obj;
         public float chance;
     }
-    
+
     public List<spawnable> spawns;
-    pub
 
 
     public void SpawnObject()
     {
-        PhotonNetwork.Instantiate(spawns[0].obj.name, Vector3.zero, Quaternion.identity);
+        PhotonNetwork.Instantiate(spawns[0].obj.name, transform.position + Vector3.zero, Quaternion.identity);
         return;
     }
 }
