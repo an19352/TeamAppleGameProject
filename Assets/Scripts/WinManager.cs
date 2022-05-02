82784b6421afe174b41b4925f349892f5a3f54c9
Kashmi12 <kashmigarg@gmail.com>
2022-05-02 06:44:04 +0100

    added skybox

--- Assets/Scripts/WinManager.cs
+++ Assets/Scripts/WinManager.cs
@@ -9,43 +9,51 @@ public class WinManager : MonoBehaviour
     public GameObject RedWins;  
     public GameObject BlueWins;  
     public GameObject scoreDraw;
-
+    public GameObject Timeout;
+    
     private void Start()
+    { StartCoroutine(Wait()); }
+    
+    IEnumerator Wait()
     {
+        yield return new WaitForSeconds(2);
+        Timeout.SetActive(false);
         PhotonRoom room = PhotonRoom.room;
-        gameObject.transform.GetChild(0).GetChild(1).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redScore.ToString();
-        gameObject.transform.GetChild(0).GetChild(1).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueScore.ToString();
-        gameObject.transform.GetChild(0).GetChild(2).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redScore.ToString();
-        gameObject.transform.GetChild(0).GetChild(2).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueScore.ToString();
-        gameObject.transform.GetChild(0).GetChild(3).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redScore.ToString();
-        gameObject.transform.GetChild(0).GetChild(3).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueScore.ToString();
-        
-        gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redFlag.ToString();
-        gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueFlag.ToString();
-        gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redFlag.ToString();
-        gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueFlag.ToString();
-        gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
-            room.redFlag.ToString();
-        gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
-            room.blueFlag.ToString();
-        
         if (room.blueFlag == room.redFlag) 
         {    
+            gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(3).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redScore.ToString();
+            gameObject.transform.GetChild(0).GetChild(3).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueScore.ToString();
             scoreDraw.SetActive(true);
-            return;
         }
-
-        if (room.redFlag > room.blueFlag) RedWins.SetActive(true);
-        else BlueWins.SetActive(true);
+        if (room.redFlag > room.blueFlag)
+        {
+            gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(1).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redScore.ToString();
+            gameObject.transform.GetChild(0).GetChild(1).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueScore.ToString();
+            RedWins.SetActive(true);
+        }
+        if (room.blueFlag > room.redFlag)
+        {
+            gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueFlag.ToString();
+            gameObject.transform.GetChild(0).GetChild(2).GetChild(1).GetChild(2).GetComponent<TMP_Text>().text =
+                room.redScore.ToString();
+            gameObject.transform.GetChild(0).GetChild(2).GetChild(1).GetChild(4).GetComponent<TMP_Text>().text =
+                room.blueScore.ToString();
+            BlueWins.SetActive(true);
+        }
     }
 }
