a4e168dabca7f7549cb16ab140e268f37b760a43
Perry Chen <2558544545qwerty@gmail.com>
2022-05-08 18:52:22 +0100

    Feat: added improved cutscene

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -9,17 +9,35 @@ public class Cutscene : MonoBehaviour
     int enemyTeam;
     GameMechanics gameMechanics;
     List<GameObject> enemyGens;
+    GameObject enemyObjective;
+    GameObject ourObjective;
     void Start()
     {
         gameMechanics = GameMechanics.gameMechanics;
         enemyTeam = gameMechanics.GetLocalPlayerTeam() == 0 ? 1 : 0;
         enemyGens = enemyTeam == 0 ? gameMechanics.redgens : gameMechanics.greengens;
+        enemyObjective = gameMechanics.flagObjectives[enemyTeam].objective;
+        ourObjective = gameMechanics.flagObjectives[gameMechanics.GetLocalPlayerTeam()].objective;
+
         int i = 0;
         foreach (Transform child in transform)
         {
-            child.GetComponent<CinemachineVirtualCamera>().Follow = enemyGens[i].transform;
-            child.GetComponent<CinemachineVirtualCamera>().LookAt = enemyGens[i].transform;
+            if (i == 3)
+            {
+                child.GetComponent<CinemachineVirtualCamera>().Follow = enemyObjective.transform;
+                child.GetComponent<CinemachineVirtualCamera>().LookAt = enemyObjective.transform;
+            }
+            else if (i == 4)
+            {
+                return;
+            }
+            else
+            {
+                child.GetComponent<CinemachineVirtualCamera>().Follow = enemyGens[i].transform;
+                child.GetComponent<CinemachineVirtualCamera>().LookAt = enemyGens[i].transform;
+            }
             i++;
         }
+
     }
 }
